3c8866a29c64fefb356d2ce2e19b9948a6ad7d4a
luo <luoxianmingg@gmail.com>
2023-07-28 14:14:28 -0400

    network page

--- app/pages/Home/index.tsx
+++ app/pages/Home/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaView, Text, View, Button, StyleSheet} from 'react-native';
-import * as Keychain from 'react-native-keychain';
-import {Toast} from 'react-native-toast-message/lib/src/Toast';
+import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
 import {BannerAd, TestIds} from 'react-native-google-mobile-ads';
 import NetInfo, {NetInfoState} from '@react-native-community/netinfo';
 
@@ -16,32 +13,36 @@ const HomePage = () => {
     });
   }, []);
   return (
-    // <NavigationContainer>
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Text>Home</Text>
-        <Text> Using this wallet in a offline device will be much safer! </Text>
-        <Text>
-          {' '}
-          Network state :{' '}
-          {networkState?.isConnected ? 'Connected' : 'Not connected'}
+        <View style={styles.textContainer}>
+          <Text style={styles.normalText}>
+            {' '}
+            Using this wallet in a offline device will be much safer!{' '}
+          </Text>
+          <View style={styles.line}>
+            <Text style={styles.lineLabel}>Network state:</Text>
+            <Text style={styles.lineText}>
+              {networkState?.isConnected ? 'Connected' : 'Not connected'}
+            </Text>
+          </View>
+        </View>
+        <Text style={styles.adsText}>
+          If you have network connection, you can see the ads below:
         </Text>
-        <Text>
-          Here is some annoying ads for you becauase you are not offline:
-        </Text>
-        <BannerAd
-          // style={{width: '100%', height: 400}}
-          unitId={TestIds.BANNER}
-          // size="600x400"
-          // size="INLINE_ADAPTIVE_BANNER"
-          size="ANCHORED_ADAPTIVE_BANNER"
-          requestOptions={{
-            requestNonPersonalizedAdsOnly: true,
-          }}
-        />
+        <View style={styles.adContainer}>
+          <BannerAd
+            unitId={TestIds.BANNER}
+            // size="600x400"
+            // size="INLINE_ADAPTIVE_BANNER"
+            size="ANCHORED_ADAPTIVE_BANNER"
+            requestOptions={{
+              requestNonPersonalizedAdsOnly: true,
+            }}
+          />
+        </View>
       </View>
     </SafeAreaView>
-    // </NavigationContainer>
   );
 };
 
@@ -52,8 +53,50 @@ const styles = StyleSheet.create({
     width: '100%',
     // padding: 20,
     flexDirection: 'column',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center',
+  },
+  normalText: {
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'left',
+    width: '100%',
+  },
+  adsText: {
+    fontSize: 14,
+    margin: 10,
+    textAlign: 'left',
+  },
+  textContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+  },
+  adContainer: {
+    flex: 2,
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  line: {
+    height: 44,
+    width: '100%',
+    paddingRight: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  lineLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'left',
+  },
+  lineText: {
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'left',
   },
 });
 export default HomePage;
